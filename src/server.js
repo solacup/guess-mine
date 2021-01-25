f97a27402ddb95e08f917eeb1269a08942c2467a
solacup <solacup07@naver.com>
2021-01-25 22:12:01 +0900

    Add socketIO

--- src/server.js
+++ src/server.js
@@ -1,3 +1,17 @@
+import { join } from "path";
 import express from "express";
+import socketIO from "socket.io";
 
-console.log('Hi');
+const PORT = 4000;
+const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () =>
+  console.log(`Server running: http://localhost:${PORT}`);
+
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO(server);
