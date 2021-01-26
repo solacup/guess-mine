31d7749c8e206d992f9dd7f0cb194107a81cf8c8
solacup <solacup07@naver.com>
2021-01-26 22:31:44 +0900

    Create Chat System

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
+app.use(logger("dev"));
 app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () =>
@@ -15,3 +17,15 @@ const handleListening = () =>
 const server = app.listen(PORT, handleListening);
 
 const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
---
+++ src/static/index.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+const sendMessage = (message) => {
+  socket.emit("newMessage", { message });
+  console.log(`You: ${message}`);
+};
+
+const setNickname = (nickname) => {
+  socket.emit("setNickname", { nickname });
+};
+
+const handleMessageNotif = (data) => {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+};
+
+socket.on("messageNotif", handleMessageNotif);
