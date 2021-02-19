1e506555d466ca643fd2f006e6189a4e61f904d1
solacup <solacup07@naver.com>
2021-02-19 22:00:29 +0900

    lecture 6.1

--- src/socketController.js
+++ src/socketController.js
@@ -1,21 +1,31 @@
 import events from "./events";
 
 const socketController = (socket) => {
-  // eslint-disable-next-line prettier/prettier
-  const broadcast = (event, data) =>
-    socket.broadcast.emit(event, data);
+  const broadcast = (event, data) => socket.broadcast.emit(event, data);
   socket.on(events.setNickname, ({ nickname }) => {
     socket.nickname = nickname;
     broadcast(events.newUser, { nickname });
   });
+
   socket.on(events.disconnect, () => {
     broadcast(events.disconnected, {
       nickname: socket.nickname,
     });
   });
-  socket.on(events.sendMsg, ({ message }) => {
-    broadcast(events.newMsg, { message, nickname: socket.nickname });
-  });
+
+  socket.on(events.sendMsg, ({ message }) =>
+    broadcast(events.newMsg, { message, nickname: socket.nickname })
+  );
+
+  socket.on(events.beginPath, ({ x, y }) =>
+    broadcast(events.beganPath, { x, y })
+  );
+
+  socket.on(events.strokePath, ({ x, y, color }) =>
+    broadcast(events.strokedPath, { x, y, color })
+  );
+
+  socket.on(events.fill, ({ color }) => broadcast(events.filled, { color }));
 };
 
 export default socketController;
